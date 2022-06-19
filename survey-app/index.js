e796d7e35eeda29d3e6315c055bafba3d25671d2
alvarogr8 <56795605+alvarogr8@users.noreply.github.com>
2022-06-19 17:17:19 +0200

    Delete survey-app directory

--- survey-app/index.js
+++
@@ -1,7 +0,0 @@
-document.getElementById("myform").addEventListener("submit", (e) => {
-  e.preventDefault();
-  const formData = new FormData(e.target);
-  const formProps = Object.fromEntries(formData);
-  console.log({ formProps });
-  alert(JSON.stringify(formProps).replace(/,|{|}/g, "\n").replace(/"/g, " "));
-});
